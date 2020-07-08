8153ede26f7d37c823a367ff40201352c93b385d
qinghuan <hashlock@163.com>
2020-07-08 16:22:07 +0800

    Fix AssetRestrictions

--- packages/types/src/chainx/AssetRestrictions.ts
+++ packages/types/src/chainx/AssetRestrictions.ts
@@ -1,4 +1,17 @@
 // eslint-disable-next-line header/header
 import U32 from '../primitive/U32';
 
-export default class AssetRestrictions extends U32 { }
+import Struct from '../codec/Struct';
+import { ExtrinsicPayloadValue, Registry } from '../types';
+
+export default class AssetRestrictions extends Struct {
+    constructor(registry: Registry, value?: ExtrinsicPayloadValue | Uint8Array | string) {
+        super(
+            registry,
+            {
+                mask: U32,
+            },
+            value
+        );
+    }
+ }
