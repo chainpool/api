60809b697745319b52007d25d9877fb079372ae0
qinghuan-chain <1245816264@qq.com>
2020-07-20 09:35:45 +0800

    [CI Skip] release/beta 0.0.3-beta.11
    skip-checks: true

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,11 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// import { detectPackage } from '@polkadot/util';
+import { detectPackage } from '@polkadot/util';
 
 // FIXME This really should be `import(...).then(...)`, but need to check rejections
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// detectPackage(require('./package.json'), typeof __dirname !== 'undefined' && __dirname);
+detectPackage(require('./package.json'), typeof __dirname !== 'undefined' && __dirname);
 
 export { Keyring } from '@polkadot/keyring';
 export { WsProvider } from '@chainx-v2/rpc-provider';
