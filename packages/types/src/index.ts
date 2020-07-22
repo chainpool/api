f22ca2962833e6f91a147e8826a031e2bb9304b7
qinghuan-chain <1245816264@qq.com>
2020-07-22 17:17:27 +0800

    [CI Skip] release/beta 0.0.3-beta.21
    skip-checks: true

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,11 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { detectPackage } from '@chainx-v2/util';
+// import { detectPackage } from '@chainx-v2/util';
 
 // FIXME This really should be `import(...).then(...)`, but need to check rejections
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-detectPackage(require('./package.json'), typeof __dirname !== 'undefined' && __dirname);
+// detectPackage(require('./package.json'), typeof __dirname !== 'undefined' && __dirname);
 
 export * from './codec';
 export * from './create';
