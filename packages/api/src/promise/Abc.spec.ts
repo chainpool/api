556dc2f6b542cbaab454b8a47fc815821657e6c2
Yongfeng LI <wliyongfeng@gmail.com>
2020-07-30 12:14:35 +0800

    Add transfer and bond test script

--- packages/api/src/promise/Abc.spec.ts
+++ packages/api/src/promise/Abc.spec.ts
@@ -4,21 +4,45 @@
 
 import ApiPromise from './Api';
 import WsProvider from "@chainx-v2/rpc-provider/ws";
+import {encodeAddress} from '@chainx-v2/keyring';
+import testKeyring, {PAIRS} from '@chainx-v2/keyring/testing';
+// import {ISubmittableResult} from "@chainx-v2/types/types";
 
+console.log(encodeAddress)
 console.log(ApiPromise);
 
 describe('ApiPromise', (): void => {
   let provider: WsProvider;
+  let api: any;
+  let keyring: any;
+  let alice: any;
 
   beforeEach((): void => {
     jest.setTimeout(300000000000000);
     provider = new WsProvider('ws://47.114.131.193:9000');
-    console.log(provider);
+
+    keyring = testKeyring()
+    alice = keyring.pairs[0]
   });
 
+  // @ts-ignore
+  async function transferFromAliceToBob() {
+    const bobAddr = encodeAddress(PAIRS[1].publicKey)
+    let extrinsic = await api.tx.xAssets.transfer(bobAddr, 0, 1000, 'alice to bob')
+    extrinsic = extrinsic.sign(alice)
+
+    console.log(extrinsic)
+  }
+
+  // @ts-ignore
+  async function aliceStake(validators) {
+    let extrinsic = await api.tx.xStaking.bond(validators[0].account, 1000, 'stake')
+    const result = await extrinsic.signAndSend(alice)
+    console.log(result)
+  }
 
   it('should ', async function () {
-    const api = await ApiPromise.create({provider});
+    api = await ApiPromise.create({provider});
     console.log(api);
 
     // @ts-ignore
@@ -28,6 +52,15 @@ describe('ApiPromise', (): void => {
     // @ts-ignore
     const validators = await api.rpc.xstaking.getValidators()
     console.log(validators.toJSON())
+
+    const aliceAddr = encodeAddress(PAIRS[0].publicKey)
+    console.log(aliceAddr)
+
+    const balance = await api.query.xAssets.assetBalance(aliceAddr, 0)
+    console.log(balance)
+
+    // await transferFromAliceToBob()
+    await aliceStake(validators.toJSON())
 
     expect(1).toEqual(1)
   });
